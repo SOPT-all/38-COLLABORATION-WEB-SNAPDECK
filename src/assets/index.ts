e92a7bddd78e358849d464cf3b10126297763a7f
nohyewon <yw4913@naver.com>
2026-05-10 22:55:26 +0900

    아이콘 SVG asset 및 export 구조 추가

---
+++ src/assets/index.ts
@@ -0,0 +1,30 @@
+// arrow
+export { default as ArrowUpIcon } from './svg/icn_arrow_up.svg?react'
+export { default as LeftIcon } from './svg/icn_left.svg?react'
+export { default as RightIcon } from './svg/icn_right.svg?react'
+export { default as RightSmallGrayIcon } from './svg/icn_right_small_grey.svg?react'
+export { default as RightSmallWhiteIcon } from './svg/icn_right_small_white.svg?react'
+
+// chevron
+export { default as DownIcon } from './svg/icn_down.svg?react'
+export { default as DropdownIcon } from './svg/icn_dropdown.svg?react'
+export { default as UpIcon } from './svg/icn_up.svg?react'
+export { default as CountdownIcon } from './svg/icn_countdown.svg?react'
+export { default as CountupIcon } from './svg/icn_countup.svg?react'
+
+// action
+export { default as PlusIcon } from './svg/icn_plus.svg?react'
+export { default as DeleteIcon } from './svg/icn_delete.svg?react'
+export { default as CheckIcon } from './svg/icn_check.svg?react'
+
+// common
+export { default as UserIcon } from './svg/icn_user.svg?react'
+export { default as CategoryIcon } from './svg/icn_categ.svg?react'
+export { default as ResearchGrayIcon } from './svg/icn_research_grey.svg?react'
+export { default as ResearchWhiteIcon } from './svg/icn_research_white.svg?react'
+export { default as NotionGrayIcon } from './svg/icn_notion_grey.svg?react'
+export { default as NotionWhiteIcon } from './svg/icn_notion_white.svg?react'
+export { default as ThunderGrayIcon } from './svg/icn_thunder_grey.svg?react'
+export { default as ThunderWhiteIcon } from './svg/icn_thunder_white.svg?react'
+export { default as WebGrayIcon } from './svg/icn_web_grey.svg?react'
+export { default as WebWhiteIcon } from './svg/icn_web_white.svg?react'
