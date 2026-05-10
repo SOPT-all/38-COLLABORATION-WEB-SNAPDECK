9720a4e6c62a383b500669793b239934c5f7aeee
seojin15 <leogin3315@gmail.com>
2026-05-11 05:21:07 +0900

    chore: eslint&prettier 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,55 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslin-config-prettier";
+import prettierPlugin, { rules } from "eslint-plugin-prettier";
+import importPlugin from "eslint-plugin-import";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist", "node-modules"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
     languageOptions: {
+      ecmaVersion: "latest",
       globals: globals.browser,
     },
+    plugins: {
+      prettier: prettierPlugin,
+      import: importPlugin,
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "prettier/prettier": error,
+
+      curly: ["warn", "all"],
+      "no-console": "warn",
+
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [["^react", "^@?\\w"], ["^@/"], ["^\\.\\./"], ["^\\./"], ["^.+\\.css$"]],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+    },
   },
-])
+]);
