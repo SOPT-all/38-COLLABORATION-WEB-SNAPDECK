8b5526c83148e99d8471798756fe640cf94fac36
nohyewon <yw4913@naver.com>
2026-05-11 23:49:06 +0900

    refactor: msw worker 실행 예외 처리 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import App from "./app/App.tsx";
 
 async function enableMocking() {
   if (import.meta.env.DEV && import.meta.env.VITE_ENABLE_MSW === "true") {
-    const { worker } = await import("@/shared/mocks/browser.ts");
+    try {
+      const { worker } = await import("@/shared/mocks/browser.ts");
 
-    await worker.start({
-      onUnhandledRequest: "warn",
-    });
+      await worker.start({
+        onUnhandledRequest: "warn",
+      });
+    } catch (error) {
+      console.error("MSW 실행 실패:", error);
+    }
   }
 }
 
