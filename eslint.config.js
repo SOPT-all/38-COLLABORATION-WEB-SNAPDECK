0f07f9f83f48006afafaeadef51a154fa2b14813
jyeon03 <bjlyeon2003@naver.com>
2026-05-11 17:01:55 +0900

    fix: Tailwind 및 Import 자동 정렬 플러그인 추가, VSCode 세팅 고도화

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,13 @@
 import js from "@eslint/js";
-import globals from "globals";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
-import prettierConfig from "eslin-config-prettier";
-import prettierPlugin, { rules } from "eslint-plugin-prettier";
-import importPlugin from "eslint-plugin-import";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default defineConfig([
-  globalIgnores(["dist", "node-modules"]),
+  globalIgnores(["dist", "node_modules"]),
   {
     files: ["**/*.{ts,tsx}"],
     extends: [
@@ -18,23 +15,14 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      prettierConfig,
     ],
     languageOptions: {
       ecmaVersion: "latest",
       globals: globals.browser,
     },
-    plugins: {
-      prettier: prettierPlugin,
-      import: importPlugin,
-      "simple-import-sort": simpleImportSort,
-    },
     rules: {
-      "prettier/prettier": error,
-
       curly: ["warn", "all"],
       "no-console": "warn",
-
       "@typescript-eslint/no-unused-vars": [
         "warn",
         {
@@ -42,14 +30,7 @@ export default defineConfig([
           varsIgnorePattern: "^_",
         },
       ],
-
-      "simple-import-sort/imports": [
-        "error",
-        {
-          groups: [["^react", "^@?\\w"], ["^@/"], ["^\\.\\./"], ["^\\./"], ["^.+\\.css$"]],
-        },
-      ],
-      "simple-import-sort/exports": "error",
     },
   },
+  eslintPluginPrettierRecommended,
 ]);
