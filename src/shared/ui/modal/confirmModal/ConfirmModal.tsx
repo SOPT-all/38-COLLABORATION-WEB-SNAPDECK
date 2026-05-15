4dd4eb6d71ceb839f102314a6f5aa4da425e466a
jyeon03 <bjlyeon2003@naver.com>
2026-05-16 01:51:07 +0900

    feat: ConfirmModal 공통 컴포넌트 구현

---
+++ src/shared/ui/modal/confirmModal/ConfirmModal.tsx
@@ -0,0 +1,79 @@
+import { DeleteIcon } from "@/assets";
+import IconButton from "@/shared/ui/iconButton";
+import Modal from "@/shared/ui/modal/Modal";
+import TextButton from "@/shared/ui/textButton";
+
+interface ConfirmModalProps {
+  isOpen: boolean;
+  title: string;
+  description: string;
+  handleOpenChange: (open: boolean) => void;
+  handleConfirm: () => void;
+  cancelText: string;
+  confirmText: string;
+}
+
+const ConfirmModal = ({
+  isOpen,
+  title,
+  description,
+  handleOpenChange,
+  handleConfirm,
+  cancelText,
+  confirmText,
+}: ConfirmModalProps) => {
+  return (
+    <Modal open={isOpen} onOpenChange={handleOpenChange}>
+      <Modal.Content className="w-[33.6rem] px-[1.8rem] pt-[2.1rem] pb-[1.1rem]">
+        <div className="flex flex-col gap-[2.25rem]">
+          <div>
+            <div className="mb-[1.25rem] flex items-center justify-between">
+              <Modal.Title className="typo-head-sb-16 text-snapdeck-500">
+                {title}
+              </Modal.Title>
+
+              <Modal.Close>
+                <IconButton
+                  variant="ghost"
+                  tone="neutral"
+                  radius="sm"
+                  iconSize="lg"
+                  aria-label="모달 닫기"
+                >
+                  <DeleteIcon />
+                </IconButton>
+              </Modal.Close>
+            </div>
+
+            <Modal.Description className="typo-caption-r-10 text-snapdeck-400 whitespace-pre-line">
+              {description}
+            </Modal.Description>
+          </div>
+
+          <div className="flex justify-end gap-[0.7rem]">
+            <Modal.Close>
+              <TextButton
+                variant="neutral"
+                size="xs"
+                className="typo-caption-r-10"
+              >
+                {cancelText}
+              </TextButton>
+            </Modal.Close>
+
+            <TextButton
+              variant="danger"
+              size="xs"
+              className="typo-caption-r-10"
+              onClick={handleConfirm}
+            >
+              {confirmText}
+            </TextButton>
+          </div>
+        </div>
+      </Modal.Content>
+    </Modal>
+  );
+};
+
+export default ConfirmModal;
