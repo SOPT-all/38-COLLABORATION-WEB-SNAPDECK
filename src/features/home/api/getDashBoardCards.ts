e61ed01e7780e7da79cca52d9c66358a75b363ba
seojin15 <leogin3315@gmail.com>
2026-05-17 22:00:35 +0900

    fix: api 수정

--- src/features/home/api/getDashBoardCards.ts
+++ src/features/home/api/getDashBoardCards.ts
@@ -2,5 +2,5 @@ import type { DashBoardCardData } from "@/features/home/types/dashboard";
 import { http } from "@/shared/api/http";
 
 export const getDashBoardCards = () => {
-  return http.get<DashBoardCardData[]>("/api/dashboards");
+  return http.get<DashBoardCardData[]>("/api/v1/sample-slides");
 };
--- src/shared/mocks/dashBoardHandler.ts
+++ src/shared/mocks/dashBoardHandler.ts
@@ -3,7 +3,7 @@ import { HttpResponse, http } from "msw";
 import { mockDashBoardCardsResponse } from "./mockDashBoardCards";
 
 export const dashBoardHandlers = [
-  http.get("/api/dashboards", () => {
+  http.get("/api/v1/sample-slides", () => {
     return HttpResponse.json(mockDashBoardCardsResponse);
   }),
 ];
