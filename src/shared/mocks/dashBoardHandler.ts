7315a966267569cf54b9e53e6a44fc6d4e3c5247
jyeon03 <bjlyeon2003@naver.com>
2026-05-20 02:08:10 +0900

    refactor: MSW 관련 코드 및 파일 제거

--- src/shared/mocks/dashBoardHandler.ts
+++
@@ -1,9 +0,0 @@
-import { HttpResponse, http } from "msw";
-
-import { mockDashBoardCardsResponse } from "./mockDashBoardCards";
-
-export const dashBoardHandlers = [
-  http.get("/api/v1/sample-slides", () => {
-    return HttpResponse.json(mockDashBoardCardsResponse);
-  }),
-];
